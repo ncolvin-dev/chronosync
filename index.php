5447fa2b717b35a8f2935911b6085589b30c57ae
Nicholas Colvin <101622069+ncolvin-dev@users.noreply.github.com>
2026-03-12 16:06:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 echo "<h1>Server is working!</h1>";
-echo "<h1>This is a webhook test!!!</h1>";
+echo "<h1>This is a webhook test!!!!!</h1>";
 echo "<p>PHP version: " . phpversion() . "</p>";
 echo "<p>Server time: " . date('Y-m-d H:i:s') . "</p>";
 
 ?>
+
 
